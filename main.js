45767c87f40ee6b37acb85271b12112f13bcb4fc
vanshika1708 <sharmavanshika1708@gmail.com>
2024-09-07 22:25:25 +0530

    minor change

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const deg = 6;
 const hr = document.querySelector('#hr');
 const mn = document.querySelector('#mn');
 const sc = document.querySelector('#sc');
-
+//set interval function
 setInterval(() => {
     let day = new Date();    
     let hh = day.getHours() * 30;   
